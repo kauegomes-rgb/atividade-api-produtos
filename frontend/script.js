a510887b47be9a97fa9c978644c262a5fd80e4b1
kauegomes-rgb <kaue.gomes@ba.estudante.senai.br>
2026-09-23 19:17:03 +0000

    cria API de produtos

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 async function carregarDados() {
 
-    const url = "http://localhost:3000/";
+    const url = "https://orange-space-rotary-phone-v6xpr747944jfwq7r-5500.app.github.dev/frontend/";
 
     const resposta = await fetch(url);
 
